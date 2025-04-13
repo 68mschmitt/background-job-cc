99e3b15ac210c25f70cb98c37ae94c76b8cd8524
68mschmitt <68mschmitt@gmail.com>
2025-04-13 13:18:37 -0400

    Git init

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace BackgroundJobCodingChallenge
+{
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+            ArgumentNullException.ThrowIfNull(args);
+            // No need to write code here!
+        }
+	}
+}
